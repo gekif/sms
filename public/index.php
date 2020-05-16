7991eaa94afc860bf74392ede10a5fd138afb67b
Gekif <dzulfikar.maulana@gmail.com>
2020-05-16 15:09:09 +0700

    2. Let's start coding: 6. Sending a message part 1

--- public/index.php
+++ public/index.php
@@ -1,5 +1,26 @@
 <?php require '../includes/header.php'; ?>
 
-<h1>Home Page</h1>
+<?php
+
+if (isset($_POST['submit'])) {
+    if (isset($_POST['number']) && isset($_POST['message'])) {
+        $client = new Services_Twilio($config['account_sid'], $config['auth_token']);
+    }
+}
+
+?>
+
+<div class="col-sm-6 col-sm-offset-3">
+    <form role="form" method="post">
+        <div class="form-group">
+                <label for="email">Phone Number</label>
+                <input name="number" type="tel" class="form-control" id="email" required placeholder="Enter number">
+            </div>
+        <div class="form-group">
+                <textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+        <input name="submit" type="submit" class="btn btn-primary btn-block" value="Send Message">
+    </form>
+</div>
 
 <?php require '../includes/footer.php'; ?>
