ca4ff980978672e85937ef90fb2fb887eb437558
Gekif <dzulfikar.maulana@gmail.com>
2020-05-16 15:28:54 +0700

    2. Let's start coding: 8. Sending a message part 2

--- app/config.php
+++ app/config.php
@@ -2,5 +2,6 @@
 
 $config = [
     'account_sid' => 'ACa8bf24355ff6de567862661bc958fee6',
-    'auth_token' => '874f42c211968044520a071f22c7005d'
+    'auth_token' => '874f42c211968044520a071f22c7005d',
+    'phone_number' => '+12073832327'
 ];
