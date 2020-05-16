a26a0e4244008763036f59166b53fad3aecd7648
Gekif <dzulfikar.maulana@gmail.com>
2020-05-16 16:08:31 +0700

    2. Let's start coding: 10. Verification code insertion in database

--- app/config.php
+++ app/config.php
@@ -1,6 +1,13 @@
 <?php
 
-$config = [
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASS") ? null : define("DB_PASS", "admin");
+defined("DB_NAME") ? null : define("DB_NAME", "sms");
+
+$con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+return $config = [
     'account_sid' => 'ACa8bf24355ff6de567862661bc958fee6',
     'auth_token' => '4e0d7af0729f8c4d202e67f86e652bb2',
     'phone_number' => '+12073832327'
--- public/code.php
+++ public/code.php
@@ -5,6 +5,8 @@
 $client = new Services_Twilio($config['account_sid'], $config['auth_token']);
 $_SESSION['code'] = $code = uniqid();
 
+$con->query("INSERT INTO verifications(code) VALUES('$code')");
+
 $client->account->messages->sendMessage($config['phone_number'], '+6281286356605', $code);
 //$client->account->sms_message->create($config['phone_number'], '+6281286356605', $code);
 
